b735db67d34ff32befbf72d137058509a7639cb0
Satyajit Kumar [ROOT] <31186982+satya-kr@users.noreply.github.com>
2023-12-09 20:59:32 +0530

    Update 01_solve-me-first.go

--- 01_solve-me-first.go
+++ 01_solve-me-first.go
@@ -2,11 +2,11 @@ package main
 
 import "fmt"
 
-func solveMeFirst(a uint32, b uint32) uint32 {
+func solveMeFirst(a, b uint32) uint32 {
 	return a + b
 }
 
 func main() {
-	var res uint32 = solveMeFirst(1, 2)
+	res  := solveMeFirst(1, 2)
 	fmt.Println(res)
 }
